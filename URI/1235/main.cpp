86f03d34c1dafcbaf8c147599491cd574e0a2a6c
vicnetto <victorhmnetto@gmail.com>
2020-11-25 11:58:12 +0100

    ADDED: 1235 (URI)

---
+++ URI/1235/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int n;
+    char newline;
+
+    scanf("%d", &n);
+    scanf("%c", &newline);
+
+    for(int i = 0; i < n; i++) {
+        string phrase;
+        getline(cin, phrase);
+
+        if (phrase.size() % 2 == 1)
+            phrase.insert(phrase.begin() + (phrase.size() / 2), ' ');
+
+        for (int j = phrase.size() / 2 - 1; j >= 0; j--)
+            cout << phrase.at(j);
+        
+        for (int j = phrase.size() - 1; j >= phrase.size() / 2; j--)
+            cout << phrase.at(j);
+
+        cout << endl;
+    }
+
+
+
+    return 0;
+}
